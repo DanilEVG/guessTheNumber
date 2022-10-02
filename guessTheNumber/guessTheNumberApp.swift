21020c772dfc20e793b440fefb7793dcd97e4ac4
DanilEVG <89922067067@mail.ru>
2022-10-02 18:57:51 +0500

    Initial Commit

---
+++ guessTheNumber/guessTheNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessTheNumberApp.swift
+//  guessTheNumber
+//
+//  Created by Даниил Евгеньевич on 02.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct guessTheNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
